ece216de045ce11729f8df0df4ed6f77601dbe28
KGrigory <96226501+KGrigory@users.noreply.github.com>
2021-12-27 15:21:47 +0500

    Update HomeWork3.java

--- HomeWork3.java
+++ HomeWork3.java
@@ -1,7 +1,7 @@
 /*
  * Java. Level 1. Lesson 3. HomeWork3
  * @autor Grigory Kondratyev
- * @version 22.12.2021
+ * @version 22.12.2021 - 27.12.2021
  */
 import java.util.Arrays;
 
@@ -13,6 +13,9 @@ class HomeWork3 {
         fillDiagonal();
 		System.out.println(Arrays.toString(retLenArr(5, 10)));
     }
+	/*
+	Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+        */
     static void invertArray() {
         int[]arr = {0,1,1,0,1,0,1,0,1,0};
         System.out.println(Arrays.toString(arr));
@@ -21,6 +24,9 @@ class HomeWork3 {
         }
         System.out.println(Arrays.toString(arr));
     }
+	/*
+	Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
+	*/
     static void filArray() {
         int[]arr = new int[100];
         for (int i = 0; i < arr.length; i++) {
@@ -28,6 +34,9 @@ class HomeWork3 {
         }
         System.out.println(Arrays.toString(arr));
     }
+	/*
+	Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+	*/
     static void changeArray() {
         int [] arr = {2, 5 , 67, 8, 3, 4, 1, 7, 6, 9};
         System.out.println(Arrays.toString(arr));
@@ -38,6 +47,12 @@ class HomeWork3 {
         }
         System.out.println(Arrays.toString(arr));
     }
+	/*
+	Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+	и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно). 
+	Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
+	*/
+	
     static void fillDiagonal() {
         int [][] arr = new int [6][6];
         for (int i = 0; i <arr.length; i++){
@@ -45,6 +60,10 @@ class HomeWork3 {
             System.out.println(Arrays.toString(arr[i]));
         }
     }
+	/*
+	Написать метод, принимающий на вход два аргумента: len и initialValue,
+	и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue;
+        */
     static int[] retLenArr (int len, int initialValue){
         int arr[] = new int[len];
         for (int i = 0; i < arr.length; i++){
